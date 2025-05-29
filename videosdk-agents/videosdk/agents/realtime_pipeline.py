15fe6e70fed46283d20363bf07ea6bc7c7a21889
uday <udaygajera116@gmail.com>
2025-05-29 12:41:33 +0530

    route audio to meeting

--- videosdk-agents/videosdk/agents/realtime_pipeline.py
+++ videosdk-agents/videosdk/agents/realtime_pipeline.py
@@ -54,11 +54,9 @@ class RealTimePipeline(Pipeline, EventEmitter[Literal[
             videosdk_auth = kwargs.get('videosdk_auth')
             meeting_id = kwargs.get('meeting_id')
             name = kwargs.get('name')
-            token = kwargs.get('token')
             self.room = VideoSDKHandler(
-                auth_token=videosdk_auth,
                 meeting_id=meeting_id,
-                auth_token=token,
+                auth_token=videosdk_auth,
                 name=name,
                 pipeline=self,
                 loop=self.loop
