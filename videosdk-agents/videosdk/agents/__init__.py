2d9dfb9cbc75c01f3499f25e3ff4859f14c30be0
uday <udaygajera116@gmail.com>
2025-05-29 11:27:25 +0530

    fix event emitter in aws plugin

--- videosdk-agents/videosdk/agents/__init__.py
+++ videosdk-agents/videosdk/agents/__init__.py
@@ -3,7 +3,7 @@ from .agent_session import AgentSession
 from .conversation_flow import ConversationFlow
 from .realtime_base_model import RealtimeBaseModel
 from .realtime_pipeline import RealTimePipeline
-from .utils import function_tool, is_function_tool, get_tool_info, FunctionTool, FunctionToolInfo, build_openai_schema, build_gemini_schema, ToolChoice
+from .utils import function_tool, is_function_tool, get_tool_info, FunctionTool, FunctionToolInfo, build_openai_schema, build_gemini_schema, ToolChoice, build_nova_sonic_schema
 from .room.audio_stream import CustomAudioStreamTrack
 from .event_emitter import EventEmitter
 from .job import WorkerJob
@@ -44,4 +44,5 @@ __all__ = [
     'global_event_emitter',
     'EventTypes',
     'CascadingPipeline',
+    'build_nova_sonic_schema',
 ]
