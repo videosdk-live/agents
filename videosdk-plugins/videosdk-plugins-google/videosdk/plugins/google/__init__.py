7ca31f8a7ad48bbe26b1c097fdcd6255fe7bbd97
Deep Bhupatkar <bhupatkardeep@gmail.com>
2025-06-09 15:58:15 +0530

    Add support for Google's LLM

--- videosdk-plugins/videosdk-plugins-google/videosdk/plugins/google/__init__.py
+++ videosdk-plugins/videosdk-plugins-google/videosdk/plugins/google/__init__.py
@@ -1,9 +1,11 @@
 from .live_api import GeminiRealtime, GeminiLiveConfig
 from .tts import GoogleTTS, GoogleVoiceConfig
+from .llm import GoogleLLM
 
 __all__ = [
     "GeminiRealtime",
     "GeminiLiveConfig",
     "GoogleTTS",
-    "GoogleVoiceConfig"
+    "GoogleVoiceConfig",
+    "GoogleLLM"
 ]
