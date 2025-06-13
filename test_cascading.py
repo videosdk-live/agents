8dfe1633a09165b83e3dc92c31b964ade73bef10
Deep Bhupatkar <144551847+DeepBhupatkar@users.noreply.github.com>
2025-06-13 10:11:11 +0530

    Add plugin support for STT, LLM, and TTS using Google and SarvamAI (#11)

--- test_cascading.py
+++ test_cascading.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 from typing import AsyncIterator
 from videosdk.plugins.openai import OpenAIRealtime, OpenAIRealtimeConfig, OpenAILLM, OpenAISTT, OpenAITTS
-from videosdk.plugins.google import GeminiRealtime, GeminiLiveConfig
+from videosdk.plugins.google import GeminiRealtime, GeminiLiveConfig, GoogleTTS,GoogleVoiceConfig,GoogleLLM, GoogleSTT
 from videosdk.plugins.deepgram import DeepgramSTT
 from videosdk.plugins.silero import SileroVAD
 from videosdk.agents import Agent, AgentSession, CascadingPipeline, function_tool, WorkerJob, MCPServerStdio, MCPServerHTTP, ConversationFlow, ChatRole
@@ -14,6 +14,7 @@ import pathlib
 import sys
 from videosdk.plugins.turn_detector import TurnDetector, pre_download_model
 from videosdk.plugins.elevenlabs import ElevenLabsTTS
+from videosdk.plugins.sarvamai import SarvamAITTS, SarvamAILLM,SarvamAISTT
 
 logger = logging.getLogger(__name__)
 
@@ -239,10 +240,17 @@ async def test_connection(jobctx):
     agent = MyVoiceAgent()
     conversation_flow = MyConversationFlow(agent)
     pipeline = CascadingPipeline(
-        stt= DeepgramSTT(api_key=os.getenv("DEEPGRAM_API_KEY")),
+        # stt= DeepgramSTT(api_key=os.getenv("DEEPGRAM_API_KEY")),
         # stt= OpenAISTT(api_key=os.getenv("OPENAI_API_KEY")),
-        llm=OpenAILLM(api_key=os.getenv("OPENAI_API_KEY")),
-        tts=OpenAITTS(api_key=os.getenv("OPENAI_API_KEY")),
+        # llm=OpenAILLM(api_key=os.getenv("OPENAI_API_KEY")),
+        # tts=OpenAITTS(api_key=os.getenv("OPENAI_API_KEY")),
+        # tts=ElevenLabsTTS(api_key=os.getenv("ELEVENLABS_API_KEY")),
+        # stt = GoogleSTT( model="latest_long"),
+        # llm=GoogleLLM(api_key=os.getenv("GOOGLE_API_KEY")),
+        # tts=GoogleTTS(api_key=os.getenv("GOOGLE_API_KEY")),
+        stt=SarvamAISTT(api_key=os.getenv("SARVAMAI_API_KEY")),
+        llm=SarvamAILLM(api_key=os.getenv("SARVAMAI_API_KEY")),
+        tts=SarvamAITTS(api_key=os.getenv("SARVAMAI_API_KEY")),
         vad=SileroVAD(),
         turn_detector=TurnDetector(threshold=0.8)
     )
