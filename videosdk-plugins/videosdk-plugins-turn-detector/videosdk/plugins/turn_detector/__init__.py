8e9c5715190180e9c957457dd006ddb797524c9d
uday <udaygajera116@gmail.com>
2025-07-09 15:43:18 +0530

    fix: change default turn detector model

--- videosdk-plugins/videosdk-plugins-turn-detector/videosdk/plugins/turn_detector/__init__.py
+++ videosdk-plugins/videosdk-plugins-turn-detector/videosdk/plugins/turn_detector/__init__.py
@@ -1,4 +1,4 @@
-from .turn_detector import TurnSenseEOU, pre_download_turn_sense
-from .turn_detector_v2 import TurnDetector, pre_download_model
+from .turn_detector import TurnDetector, pre_download_model
+from .turn_detector_v2 import VideoSDKTurnDetector, pre_download_videosdk_model
 
-__all__ = ["TurnSenseEOU", "TurnDetector", "pre_download_turn_sense", "pre_download_model"]
+__all__ = ["TurnDetector", "VideoSDKTurnDetector", "pre_download_model", "pre_download_videosdk_model"]
