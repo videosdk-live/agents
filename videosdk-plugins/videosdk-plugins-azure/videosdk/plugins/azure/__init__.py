e98d9608610bd1d989b624f418cdff3b7038fb82
Deep Bhupatkar <bhupatkardeep@gmail.com>
2025-09-18 12:36:00 +0530

    Add support for azure voice live (realtime)

---
+++ videosdk-plugins/videosdk-plugins-azure/videosdk/plugins/azure/__init__.py
@@ -0,0 +1,5 @@
+from .stt import AzureSTT
+from .tts import AzureTTS
+from .voice_live import AzureVoiceLive, AzureVoiceLiveConfig
+
+__all__ = ["AzureSTT", "AzureTTS", "AzureVoiceLive", "AzureVoiceLiveConfig"]
