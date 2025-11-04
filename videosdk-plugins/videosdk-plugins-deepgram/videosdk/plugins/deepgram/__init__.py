5eaf973caea2e502c8f8c423a3ba55b58e9ce71f
“Nandini <nandini.gattani09@gmail.com>
2025-11-04 12:30:39 +0530

    init.py

--- videosdk-plugins/videosdk-plugins-deepgram/videosdk/plugins/deepgram/__init__.py
+++ videosdk-plugins/videosdk-plugins-deepgram/videosdk/plugins/deepgram/__init__.py
@@ -1,3 +1,4 @@
 from .stt import DeepgramSTT
+from .stt_v2 import DeepgramSTTV2
 from .tts import DeepgramTTS
-__all__ = ["DeepgramSTT", "DeepgramTTS"]
+__all__ = ["DeepgramSTT","DeepgramSTTV2" "DeepgramTTS"]
