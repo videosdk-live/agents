ef00f7fa9e48e92df9dcda2a2bd7a9176c8f71b5
Deep Bhupatkar <144551847+DeepBhupatkar@users.noreply.github.com>
2025-12-04 17:47:19 +0530

    Agents v0.0.48 (#125)

--- videosdk-agents/videosdk/agents/room/room.py
+++ videosdk-agents/videosdk/agents/room/room.py
@@ -190,7 +190,7 @@ class VideoSDKHandler:
         self._left: bool = False
         self.sdk_metadata = {
             "sdk": "agents",
-            "sdk_version": "0.0.47"
+            "sdk_version": "0.0.48"
         }
 
         self.meeting = VideoSDK.init_meeting(
