e0788820cd8b6fbeb0d9812b4316156790345084
Deep Bhupatkar <bhupatkardeep@gmail.com>
2025-12-09 20:00:52 +0530

    updated the dtmf and voice mail example
    - with proper naming and formate

--- examples/dtmf_voicemail.py
+++ examples/dtmf_voicemail.py
@@ -1,24 +1,19 @@
-# This test script is used to test DTMF Event and Voicemailhandling.
-import asyncio
+# This test script is used to test DTMF Event and Voicemail Detection and it's handling.
 import logging
-from token import OP
-import aiohttp
-from videosdk.agents import Agent, AgentSession, CascadingPipeline, DTMFHandler, VoiceMailDetector, WorkerJob, MCPServerStdio, ConversationFlow, JobContext, RoomOptions, Options
-from videosdk.plugins.openai import OpenAILLM
+from videosdk.agents import Agent, AgentSession, CascadingPipeline, DTMFHandler, VoiceMailDetector, WorkerJob,ConversationFlow, JobContext, RoomOptions, Options
 from videosdk.plugins.deepgram import DeepgramSTT
+from videosdk.plugins.openai import OpenAILLM
+from videosdk.plugins.elevenlabs import ElevenLabsTTS
 from videosdk.plugins.silero import SileroVAD
 from videosdk.plugins.turn_detector import TurnDetector, pre_download_model
-from videosdk.plugins.elevenlabs import ElevenLabsTTS
 
-logging.getLogger().setLevel(logging.CRITICAL)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", handlers=[logging.StreamHandler()])
 pre_download_model()
-
 class VoiceAgent(Agent):
     def __init__(self):
         super().__init__(
             instructions="You are a helpful voice assistant that can answer questions."
         )
-        
     async def on_enter(self) -> None:
         await self.session.say("Hello, how can I help you today?")
     
@@ -30,8 +25,8 @@ async def entrypoint(ctx: JobContext):
     agent = VoiceAgent()
     conversation_flow = ConversationFlow(agent)
 
-    pipeline = CascadingPipeline(
-        stt= DeepgramSTT(),
+    pipeline=CascadingPipeline(
+        stt=DeepgramSTT(),
         llm=OpenAILLM(),
         tts=ElevenLabsTTS(),
         vad=SileroVAD(),
@@ -43,10 +38,10 @@ async def entrypoint(ctx: JobContext):
         await agent.hangup()
 
     async def custom_callback_dtmf(message):
-        print("Callback message received:", message)
+        print("DTMF message received:", message)
 
     dtmf_handler = DTMFHandler(custom_callback_dtmf)
-    voice_mail_detector = VoiceMailDetector(llm=OpenAILLM(), duration=7.0, callback = custom_callback_voicemail)
+    voice_mail_detector = VoiceMailDetector(llm=OpenAILLM(), duration=5.0, callback = custom_callback_voicemail)
 
     session = AgentSession(
         agent=agent, 
@@ -59,15 +54,9 @@ async def entrypoint(ctx: JobContext):
     await session.start(wait_for_participant=True, run_until_shutdown=True)
 
 def make_context() -> JobContext:
-    room_options = RoomOptions(room_id="<room_id>", name="Sandbox Agent", playground=True)
-    
-    return JobContext(
-        room_options=room_options 
-        ) 
- 
-options = Options(agent_id="<agent-id>", max_processes=5, register=True, log_level="INFO", host="localhost", port=5000) 
+    room_options = RoomOptions(name="DTMF & Voicemail Agent", playground=True)
+    return JobContext(room_options=room_options) 
  
 if __name__ == "__main__":
-    job = WorkerJob(entrypoint=entrypoint, jobctx=make_context, options=options)
+    job = WorkerJob(entrypoint=entrypoint, jobctx=make_context, options=Options(agent_id="YOUR_AGENT_ID", max_processes=2, register=True, host="localhost", port=8081))
     job.start()
-
