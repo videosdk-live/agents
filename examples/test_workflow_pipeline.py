cb590545e9350dae6d8260be23e92ec2c91ced97
“Nandini <nandini.gattani09@gmail.com>
2025-12-12 15:43:26 +0530

    room id removed

--- examples/test_workflow_pipeline.py
+++ examples/test_workflow_pipeline.py
@@ -216,7 +216,7 @@ async def entrypoint(ctx: JobContext):
     await session.start(wait_for_participant=True, run_until_shutdown=True)
 
 def make_context() -> JobContext:
-    room_options = RoomOptions(room_id="fxyg-gp30-pwqb", name="Sandbox Agent", playground=True)
+    room_options = RoomOptions(room_id="<room_id>", name="Sandbox Agent", playground=True)
     
     return JobContext(
         room_options=room_options
