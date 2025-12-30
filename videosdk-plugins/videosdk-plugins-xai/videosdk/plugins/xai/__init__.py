fe0a1f7a6c3df9fbb7249835ab8645d891ae5098
Deep Bhupatkar <bhupatkardeep@gmail.com>
2025-12-30 10:55:40 +0530

    xAI LLM Support

--- videosdk-plugins/videosdk-plugins-xai/videosdk/plugins/xai/__init__.py
+++ videosdk-plugins/videosdk-plugins-xai/videosdk/plugins/xai/__init__.py
@@ -1,3 +1,4 @@
 from .xai_realtime import XAIRealtime, XAIRealtimeConfig, XAITurnDetection
+from .llm import XAILLM
 
-__all__ = ["XAIRealtime", "XAIRealtimeConfig", "XAITurnDetection"]
+__all__ = ["XAIRealtime", "XAIRealtimeConfig", "XAITurnDetection", "XAILLM"]
