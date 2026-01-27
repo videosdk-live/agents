7d8cdf38173ccae2ea23c9a2cdbde991e05f94f6
krishna-videosdk <krishna@videosdk.live>
2026-01-27 22:37:09 +0530

    TTS Updates: ElevenLabs + Cartesia (#174)
    Co-authored-by: Deep Bhupatkar <bhupatkardeep@gmail.com>

--- videosdk-plugins/videosdk-plugins-cartesia/videosdk/plugins/cartesia/__init__.py
+++ videosdk-plugins/videosdk-plugins-cartesia/videosdk/plugins/cartesia/__init__.py
@@ -1,4 +1,4 @@
-from .tts import CartesiaTTS
+from .tts import CartesiaTTS, GenerationConfig 
 from .stt import CartesiaSTT
 
-__all__ = ["CartesiaTTS", "CartesiaSTT"]
+__all__ = ["CartesiaTTS", "CartesiaSTT", "GenerationConfig"]
