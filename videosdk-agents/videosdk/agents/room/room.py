0bfc1dd7f36f64cb5eac39cb5802a9e5d3ce5adc
Deep Bhupatkar <bhupatkardeep@gmail.com>
2026-03-20 17:07:00 +0530

    Agents v0.0.72

--- videosdk-agents/videosdk/agents/room/room.py
+++ videosdk-agents/videosdk/agents/room/room.py
@@ -214,7 +214,7 @@ class VideoSDKHandler(BaseTransportHandler):
         self._left: bool = False
         self.sdk_metadata = {
             "sdk": "agents",
-            "sdk_version": "0.0.71"
+            "sdk_version": "0.0.72"
         }
         self.videosdk_meeting_meta_data= {
             "agent_id": self.agent_id,
